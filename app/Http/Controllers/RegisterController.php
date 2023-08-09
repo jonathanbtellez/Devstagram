84b0d417ff8b3953e75ac64bc3a7aa4918439096
jonathanbtellez <jonathanbtellez@gmail.com>
2023-08-08 21:39:29 -0500

    Apply validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,6 +23,17 @@ class RegisterController extends Controller
         // dd($request);
 
         // Using get method we can obtain the info related to the name attribute in html pass as parameter
-        dd($request->get('name'));
+        // dd($request->get('name'));
+
+        // Validation
+        // Do validate of the information received
+        // validate($request, rules)
+
+        $this->validate($request, [
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:25',
+            'email' => 'required|unique:users|email',
+            'password' => 'required',
+        ]);
     }
 }
